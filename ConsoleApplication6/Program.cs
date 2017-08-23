bc466c420f293257feabd467065512b32af0a3f7
mahesaluthan <echasmith97@gmail.com>
2017-08-23 10:13:35 +0700

    console

---
+++ ConsoleApplication6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Tester
+    {
+        public void Display(params int[] intVals)
+        {
+            foreach (int i in intVals)
+            {
+                Console.WriteLine("Disp {0}", i);
+            }
+        }
+        static void Main()
+        {
+            Tester t = new Tester();
+            t.Display(5, 6, 7, 8);
+            int[] explicitArray = new int[] { 1, 2, 3, 4, 5 };
+            t.Display(explicitArray);
+
+            Console.ReadLine();
+        }
+    }
+}
